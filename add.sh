640c71182e04c446f043e4e554dc490a4a5b7299
Saulo Alves <sauloal@gmail.com>
2014-07-13 01:17:43 +0200

    interactive add

--- add.sh
+++ add.sh
@@ -1,11 +1,13 @@
 docker stop gitlab_add; docker rm gitlab_add
 
-docker run --name='gitlab_add' -d \
+docker run --name='gitlab_add' -it \
 -e "GITLAB_HTTPS=false" \
 -v $PWD/data:/home/git/data \
 -v $PWD/mysql:/var/lib/mysql \
 sameersbn/gitlab:7.0.0 \
 app:rake gitlab:import:repos
 
-watch 'docker ps -a | grep gitlab ;docker logs gitlab_add | tail -20'
+docker rm gitlab_add
+
+#watch 'docker ps -a | grep gitlab ;docker logs gitlab_add | tail -20'
 
